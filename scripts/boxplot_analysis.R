aadcc3e28297d6406255061a167ed054f7cf996a
Felipe Fronchetti <ffronchetti@lsu.edu>
2026-03-06 23:19:24 -0600

    Create boxplot_analysis.R

---
+++ scripts/boxplot_analysis.R
@@ -0,0 +1,106 @@
+library(readxl)
+
+# Image size: 270 x 140 pixels
+final_dataset <- read_excel("GitHub/rose-2026-newcomers/final_dataset.xlsx")
+
+# CONTRIBUTORS COUNT
+par(mar = c(2, 2, 1, 1), cex.axis = 1.2, cex.lab = 1.2)
+
+boxplot(
+  na.omit(final_dataset$contributors_count),
+  horizontal = TRUE,
+  col = "lightgray",
+  lwd = 3,        # Controls the box and median
+  whisklwd = 3,   # Controls the whisker lines
+  staplelwd = 3,  # Controls the "T" ends of the whiskers
+  outline = FALSE,
+  xlab = ""
+)
+
+# This adds the thick border around the plot area
+box(lwd = 3)
+
+# This redraws the x-axis line with the thicker width
+axis(side = 1, lwd = 3, labels = FALSE)
+
+# COMMITS COUNT
+par(mar = c(2, 2, 1, 1), cex.axis = 1.2, cex.lab = 1.2)
+
+boxplot(
+  na.omit(final_dataset$commits_count),
+  horizontal = TRUE,
+  col = "lightgray",
+  lwd = 3,        # Controls the box and median
+  whisklwd = 3,   # Controls the whisker lines
+  staplelwd = 3,  # Controls the "T" ends of the whiskers
+  outline = FALSE,
+  xlab = ""
+)
+
+# This adds the thick border around the plot area
+box(lwd = 3)
+
+# This redraws the x-axis line with the thicker width
+axis(side = 1, lwd = 3, labels = FALSE)
+
+# SIZE
+par(mar = c(2, 2, 1, 1), cex.axis = 1.2, cex.lab = 1.2)
+
+boxplot(
+  na.omit(final_dataset$size),
+  horizontal = TRUE,
+  col = "lightgray",
+  lwd = 3,        # Controls the box and median
+  whisklwd = 3,   # Controls the whisker lines
+  staplelwd = 3,  # Controls the "T" ends of the whiskers
+  outline = FALSE,
+  xlab = ""
+)
+
+# This adds the thick border around the plot area
+box(lwd = 3)
+
+# This redraws the x-axis line with the thicker width
+axis(side = 1, lwd = 3, labels = FALSE)
+
+
+# FORKS COUNT
+par(mar = c(2, 2, 1, 1), cex.axis = 1.2, cex.lab = 1.2)
+
+boxplot(
+  na.omit(final_dataset$`Number of forks`),
+  horizontal = TRUE,
+  col = "lightgray",
+  lwd = 3,        # Controls the box and median
+  whisklwd = 3,   # Controls the whisker lines
+  staplelwd = 3,  # Controls the "T" ends of the whiskers
+  outline = FALSE,
+  xlab = ""
+)
+
+# This adds the thick border around the plot area
+box(lwd = 3)
+
+# This redraws the x-axis line with the thicker width
+axis(side = 1, lwd = 3, labels = FALSE)
+
+# NEWCOMERS COUNT (VERTICAL)
+
+par(mar = c(2, 2, 1, 1), cex.axis = 1.2, cex.lab = 1.2)
+
+boxplot(
+  na.omit(final_dataset$`Average number of newcomers per month`),
+  col = "lightgray",
+  lwd = 3,        # Controls the box and median
+  whisklwd = 3,   # Controls the whisker lines
+  staplelwd = 3,  # Controls the "T" ends of the whiskers
+  outline = FALSE,
+  ylab = ""
+)
+
+# Thick border around the plot area
+box(lwd = 3)
+
+# Redraw the y-axis thicker
+axis(side = 2, lwd = 3, labels = FALSE)
+
